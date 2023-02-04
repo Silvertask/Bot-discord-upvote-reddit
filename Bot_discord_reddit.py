224d39e8fe05711584426a9713bbdab5cd866aa8
Silvertask <matthieu.planchot@orange.fr>
2023-02-04 16:49:27 +0100

    maj

--- Bot_discord_reddit.py
+++ Bot_discord_reddit.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+import discord 
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+intents.message_content = True
+
+client = commands.Bot(command_prefix=',', intents=intents)
+
+@client.event
+async def on_ready():
+    print("hello world")
+
+@client.event 
+async def on_message(message):
+    if message.content.startswith("/upvote"):
+        print(message.author.name)
+        if message.author.name != "Bot_reddit":
+            print(message.content)
+    
+
+client.run("MTA3MTQ0NzA3NjMwODc5NTQ4Mg.G84v67.g5QV9cHMoMc_udH9B3wjhO-q--MloE9eemcnxI")
