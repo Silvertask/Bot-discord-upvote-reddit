0b50ea6e28dd62d73a90ee6e39a076c339de6f0a
Silvertask <matthieu.planchot@orange.fr>
2023-02-07 22:45:10 +0100

    maj

--- Bot_discord_reddit.py
+++ Bot_discord_reddit.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-import Config_bot_reddit
-import discord 
-from discord.ext import commands
+import Config_bot_reddit #Import le fichier suivant 
+import discord #import la biblio discord
+from discord.ext import commands #Permet d'utiliser les commades de la biblio 
+
 
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 intents.message_content = True
+
+import requests
+
+url = "https://www.reddit.com/api/v1/posts/top?limit=1"
+response = requests.get(url)
 
 client = commands.Bot(command_prefix=',', intents=intents)
 
@@ -18,8 +24,17 @@ async def on_ready():
 @client.event 
 async def on_message(message):
     if message.content.startswith("/upvote"):
-        print(message.author.name)
-        if message.author.name != "Bot_reddit":
+            if response.status_code == 200:
+                data = response.json()
+                post = data["data"][0]
+                title = post["title"]
+                subreddit = post["subreddit"]
+                score = post["score"]
+            print("The most upvoted post on Reddit is '{}' from r/{} with a score of {}.".format(title, subreddit, score))
+    else:
+            print("Request failed with status code: {}".format(response.status_code))
+    
+    if message.author.name != "Bot_reddit":
             print(message.content)
     
 
